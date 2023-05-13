71c41112f8eabf40a4b974428e8150c3e67c7eb3
Carles Costas <carles_cosmat@hotmail.es>
2023-05-13 03:56:53 +0200

    AppiCalls

--- app/app.py
+++ app/app.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+socketio = SocketIO(app,static_folder='build')
 
 players = {}
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
+@app.route('/connect', methods=['POST'])
+def connect():
+    username = request.json['username']
+    room = request.json['room']
+    isNewRoom = request.json['isNewRoom']
 
+@app.route('/disconnect', methods=['DELETE'])
+def disconnect():
+    username = request.json['username']
+    
+@app.route('/room/<room_id>', methods=['GET'])
+def get_room(room_id):
+    # Aquí iría la lógica para obtener la información de la sala con el ID especificado
+    return jsonify({'room_id': room_id, 'info': 'información de la sala'})
+   
 @socketio.on('message')
 def handleMessage(msg):
     print("Message: "+msg)
 """
+@app.route('/connect', methods=['POST'])
+def connect():
+    username = request.json['username']
+    room = request.json['room']
+    
 return render_template('/index.html')
 @app.route('/game')
 def game():
